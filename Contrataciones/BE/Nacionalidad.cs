575cdc53a39d0f73a26e732d85b52fd1b2bf1f68
LFranCarreira <carreirafranco@gmail.com>
2025-09-03 21:23:55 -0300

    Feature: Objetos BE definidos

---
+++ Contrataciones/BE/Nacionalidad.cs
@@ -0,0 +1,21 @@
+﻿namespace BE
+{
+    public class Nacionalidad
+    {
+        private int idNacionalidad;
+
+        public int IdNacionalidad
+        {
+            get { return idNacionalidad; }
+            set { idNacionalidad = value; }
+        }
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+    }
+}
