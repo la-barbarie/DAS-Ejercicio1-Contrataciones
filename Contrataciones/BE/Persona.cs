052510010edbc0be2f9ea472902079a9d5b3d045
Ramiro <RamiroNicolas.Mendoza@alumnos.uai.edu.ar>
2025-09-07 20:42:32 -0300

    Simplificar propiedades en Nacionalidad, Persona y Profesion

--- Contrataciones/BE/Persona.cs
+++ Contrataciones/BE/Persona.cs
@@ -2,60 +2,12 @@
 {
     public class Persona
     {
-		private int numeroPersona;
-
-		public int NumeroPersona
-		{
-			get { return numeroPersona; }
-			set { numeroPersona = value; }
-		}
-
-		private string nombre;
-
-		public string Nombre
-		{
-			get { return nombre; }
-			set { nombre = value; }
-		}
-        
-		private string apellido;
-
-        public string Apellido
-        {
-            get { return apellido; }
-            set { apellido = value; }
-        }
-
-        private int edad;
-
-		public int Edad
-		{
-			get { return edad; }
-			set { edad = value; }
-		}
-
-        private string sexo;
-
-        public string Sexo
-        {
-            get { return sexo; }
-            set { sexo = value; }
-        }
-
-        private  Nacionalidad nacionalidad;
-
-        public Nacionalidad Nacionalidad
-        {
-            get { return nacionalidad; }
-            set { nacionalidad = value; }
-        }
-
-        private Profesion profesion;
-
-        public Profesion Profesion
-        {
-            get { return profesion; }
-            set { profesion = value; }
-        }
+		public int NumeroPersona { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+        public bool Sexo { get; set; }
+        public int Nacionalidad { get; set; }
+        public int Profesion { get; set; }
     }
 }
