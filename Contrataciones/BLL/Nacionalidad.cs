0db054dbc1a9f22c23341447645791353b1f95bc
Kaukel <kaukebott@gmail.com>
2025-09-10 17:50:01 -0300

    Lambda Aberration Part 3: Nacionalidad

--- Contrataciones/BLL/Nacionalidad.cs
+++ Contrataciones/BLL/Nacionalidad.cs
@@ -15,32 +15,20 @@ namespace BLL
             {
 
                 DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
-
                 BE.Nacionalidad n = new BE.Nacionalidad();
 
                 if (string.IsNullOrWhiteSpace(nombre)) throw new Exception("El nombre es invalido");
                 else n.Nombre = nombre;
 
                 return mp.Insert(n);
-
             }
             catch (Exception e) { throw e; }
-
         }
-
-        public int RemoverNacionalidad(int numero)
-        {
-            DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
-            return mp.Delete(numero);
-        }
-
         public int EditarNacionalidad(int id, string nombre)
         {
             try
-            {
-
+            { 
                 DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
-
                 BE.Nacionalidad n = new BE.Nacionalidad();
 
                 n.IdNacionalidad = id;
@@ -49,35 +37,14 @@ namespace BLL
                 else n.Nombre = nombre;
 
                 return mp.Update(n);
-
             }
             catch (Exception e) { throw e; }
         }
-
-        public BE.Nacionalidad GetById(int id)
-        {
-            DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
-            return mp.FindById(id);
-        }
-
-        public List<BE.Nacionalidad> ListarNacionalidades()
-        {
-            DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
-            return mp.GetAll();
-        }
-
-        public BE.dto.PersonasNacionalidadDTO GetMinPersonasRegistradas()
-        {
-            DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
-            return mp.GetNacionalidadConMasOMenosPersonas(((int)EOrden.MIN));
-        }
-
-        public BE.dto.PersonasNacionalidadDTO GetMaxPersonasRegistradas()
-        {
-            DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
-            return mp.GetNacionalidadConMasOMenosPersonas((int)EOrden.MAX);
-        }
-
+        public int RemoverNacionalidad(int numero) => new DAL.mapper.MapperNacionalidad().Delete(numero);
+        public BE.Nacionalidad GetById(int id) => new DAL.mapper.MapperNacionalidad().FindById(id);
+        public List<BE.Nacionalidad> ListarNacionalidades() => new DAL.mapper.MapperNacionalidad().GetAll();
+        public BE.dto.PersonasNacionalidadDTO GetMinPersonasRegistradas() => new DAL.mapper.MapperNacionalidad().GetNacionalidadConMasOMenosPersonas((int)EOrden.MIN);
+        public BE.dto.PersonasNacionalidadDTO GetMaxPersonasRegistradas() => new DAL.mapper.MapperNacionalidad().GetNacionalidadConMasOMenosPersonas((int)EOrden.MAX);
     }
 }
 
