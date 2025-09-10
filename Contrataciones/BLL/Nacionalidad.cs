035c43cbfa7b109620e27c87ee5ffdff6584d762
Franco Carreira <carreirafranco@gmail.com>
2025-09-10 17:54:51 -0300

    Documentacion de BLL

--- Contrataciones/BLL/Nacionalidad.cs
+++ Contrataciones/BLL/Nacionalidad.cs
@@ -1,26 +1,27 @@
 ﻿using BE.enums;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL
 {
     public class Nacionalidad
     {
+        // Inserta una nueva nacionalidad en la base de datos
         public int InsertarNacionalidad(string nombre)
         {
             try
             {
-
+                // Instanciamos el mapper para la capa DAL
                 DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
 
+                // Creamos un objeto de negocio Nacionalidad
                 BE.Nacionalidad n = new BE.Nacionalidad();
 
+                // Validacion basica
                 if (string.IsNullOrWhiteSpace(nombre)) throw new Exception("El nombre es invalido");
                 else n.Nombre = nombre;
 
+                // Se inserta en la base de datos
                 return mp.Insert(n);
 
             }
@@ -28,12 +29,14 @@ namespace BLL
 
         }
 
+        // Elimina una nacionalidad por id
         public int RemoverProfesion(int numero)
         {
             DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
             return mp.Delete(numero);
         }
 
+        // Edita el nombre de una nacionalidad existente
         public int EditarPersona(int id, string nombre)
         {
             try
@@ -41,37 +44,44 @@ namespace BLL
 
                 DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
 
+                // Se crea el objeto con el id a modificar
                 BE.Nacionalidad n = new BE.Nacionalidad();
 
                 n.IdNacionalidad = id;
 
+                // Validacion del nuevo nombre
                 if (string.IsNullOrWhiteSpace(nombre)) throw new Exception("El nombre es invalido");
                 else n.Nombre = nombre;
 
+                // Se actualiza en la base de datos
                 return mp.Update(n);
 
             }
             catch (Exception e) { throw e; }
         }
 
+        // Obtiene una nacionalidad por id
         public BE.Nacionalidad GetById(int id)
         {
             DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
             return mp.FindById(id);
         }
 
+        // Lista todas las nacionalidades registradas
         public List<BE.Nacionalidad> ListarPersonas()
         {
             DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
             return mp.GetAll();
         }
 
+        // Obtiene la nacionalidad con menor cantidad de personas registradas
         public BE.dto.PersonasNacionalidadDTO GetMinPersonasRegistradas()
         {
             DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
             return mp.GetNacionalidadConMasOMenosPersonas(((int)EOrden.MIN));
         }
 
+        // Obtiene la nacionalidad con mayor cantidad de personas registradas
         public BE.dto.PersonasNacionalidadDTO GetMaxPersonasRegistradas()
         {
             DAL.mapper.MapperNacionalidad mp = new DAL.mapper.MapperNacionalidad();
--- Contrataciones/BLL/Personas.cs
+++ Contrataciones/BLL/Personas.cs
@@ -1,37 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using BE.dto;
 
 namespace BLL
 {
     public class Personas
     {
+        // Inserta una nueva persona en la base de datos
         public int InsertarPersona(string nombre, string apellido, int edad, bool sexo, BE.Nacionalidad nacionalidad, BE.Profesion profesion)
         {
             try
             {
-
+                // Se instancia el mapper de DAL
                 DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
 
+                // Se crea el objeto de negocio Persona
                 BE.Persona p = new BE.Persona();
 
+                // Validacion de nombre
                 if (string.IsNullOrWhiteSpace(nombre)) throw new Exception("El nombre es invalido");
                 else p.Nombre = nombre;
 
+                // Validacion de apellido
                 if (string.IsNullOrWhiteSpace(apellido)) throw new Exception("El apellido es invalido");
                 else p.Apellido = apellido;
 
-                p.Edad = Math.Abs(edad); //evitamos edades negativas
+                // Se evita que la edad sea negativa
+                p.Edad = Math.Abs(edad);
+
                 p.Sexo = sexo; //SEXO ES UN BOOL 
                 //0 - Mujer (pq son falsas y mentirosas)
                 //1 - Hombre (pq somos superiores y siempre verdaderos)
 
+                // Se asigna nacionalidad y profesion usando los ids
                 p.Nacionalidad = nacionalidad.IdNacionalidad;
                 p.Profesion = profesion.IdProfesion;
 
+                // Se inserta en la base de datos
                 return mp.Insert(p);
 
             }
@@ -39,12 +45,14 @@ namespace BLL
 
         }
 
+        // Elimina una persona por id
         public int RemoverPersona(int numero)
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             return mp.Delete(numero);
         }
 
+        // Edita los datos de una persona existente
         public int EditarPersona(int id, string nombre, string apellido, int edad, bool sexo, BE.Nacionalidad nacionalidad, BE.Profesion profesion)
         {
             try
@@ -56,62 +64,75 @@ namespace BLL
 
                 p.NumeroPersona = id;
 
+                // Validacion de nombre
                 if (string.IsNullOrWhiteSpace(nombre)) throw new Exception("El nombre es invalido");
                 else p.Nombre = nombre;
 
+                // Validacion de apellido
                 if (string.IsNullOrWhiteSpace(apellido)) throw new Exception("El apellido es invalido");
                 else p.Apellido = apellido;
 
-                p.Edad = Math.Abs(edad); //evitamos edades negativas
+                // Evitamos edades negativas
+                p.Edad = Math.Abs(edad);
+
                 p.Sexo = sexo; //SEXO ES UN BOOL 
                 //0 - Mujer (pq son falsas y mentirosas)
                 //1 - Hombre (pq somos superiores y siempre verdaderos)
 
+                // Se asigna nacionalidad y profesion
                 p.Nacionalidad = nacionalidad.IdNacionalidad;
                 p.Profesion = profesion.IdProfesion;
 
+                // Actualiza en la base de datos
                 return mp.Update(p);
 
             }
             catch (Exception e) { throw e; }
         }
 
+        // Busca una persona por id
         public BE.Persona GetById(int id)
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             return mp.FindById(id);
         }
 
+        // Lista todas las personas
         public List<BE.Persona> ListarPersonas()
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             return mp.GetAll();
         }
 
+        // Obtiene el promedio de edad de todas las personas
         public int GetPromedioEdad()
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             return mp.GetPromedioEdad();
         }
 
+        // Obtiene la cantidad total de personas
         public int GetCantidad()
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             return mp.GetCantidadPersonas();
         }
 
+        // Obtiene la edad minima registrada
         public int GetMinEdad()
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             return mp.GetMinOrMaxAge((int)BE.enums.EOrden.MIN);
         }
 
+        // Obtiene la edad maxima registrada
         public int GetMaxEdad()
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             return mp.GetMinOrMaxAge((int)BE.enums.EOrden.MAX);
         }
 
+        // Obtiene la cantidad de personas para una nacionalidad especifica
         public int GetCantidadPersonasPorNacionalidad(int id) //ID de nacionalidad
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
@@ -124,17 +145,21 @@ namespace BLL
                 cantidad++;
             }
 
+            // Retorna la cantidad de personas de esa nacionalidad
             return promedio.Where(p => p.Nacionalidad.IdNacionalidad == id).First().CantidadPersonas;
         }
 
+        // Obtiene el promedio de edad de una nacionalidad especifica
         public int GetPromedioEdadPorNacionalidad(int id) //ID de nacionalidad
         {
             DAL.mapper.MapperPersona mp = new DAL.mapper.MapperPersona();
             List<PromedioEdadNacionalidadDTO> promedio = mp.GetPromedioEdadPorNacionalidad(id);
 
+            // Retorna el promedio de edad para la nacionalidad dada
             return promedio.Where(p => p.Nacionalidad.IdNacionalidad == id).First().PromedioEdad;
 
             //medio nefasto pero veremos si funciona
+            // Nota: podria optimizarse para no recorrer dos veces la lista
         }
 
     }
